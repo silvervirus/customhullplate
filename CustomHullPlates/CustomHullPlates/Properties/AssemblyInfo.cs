893becd76f7edee3ce4973c1bb73220152f8c5bf
M3dicCookie <pred102@hotmail.com>
2020-04-27 11:25:53 -0700

    files

---
+++ CustomHullPlates/CustomHullPlates/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyTitle("CustomHullPlates")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomHullPlates")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b4872c8b-b8e8-4e04-be22-b0dd2fc7d558")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomHullPlates/CustomHullPlates/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace CustomHullPlates
+{
+	// Token: 0x02000005 RID: 5
+	public class Utility
+	{
+		// Token: 0x0600000F RID: 15 RVA: 0x00002334 File Offset: 0x00000534
+		public static Texture2D LoadTexture(string path, TextureFormat format, int width = 2, int height = 2)
+		{
+			bool flag = File.Exists(path);
+			if (flag)
+			{
+				byte[] array = File.ReadAllBytes(path);
+				Texture2D texture2D = new Texture2D(width, height, format, false);
+				bool flag2 = texture2D.LoadImage(array);
+				if (flag2)
+				{
+					return texture2D;
+				}
+			}
+			return null;
+		}
+
+		// Token: 0x06000010 RID: 16 RVA: 0x00002378 File Offset: 0x00000578
+		public static Sprite TextureToSprite(Texture2D tex, Vector2 pivot = default(Vector2), float pixelsPerUnit = 100f, SpriteMeshType spriteType = default, Vector4 border = default)
+		{
+			return Sprite.Create(tex, new Rect(0f, 0f, (float)tex.width, (float)tex.height), pivot, pixelsPerUnit, 0u, spriteType, border);
+		}
+	}
+}
